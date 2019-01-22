2a2c4aa0014859d52d0cdde42efb6ad543d8eb7f
Morgan Arvander <morgan.arvander@crosscut.se>
2019-01-22 12:33:24 +0100

    Initial commit

---
+++ Solution/WebFront/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebFront.Pages
+{
+    using System.Net.Http;
+
+    public class IndexModel : PageModel
+    {
+        public async Task OnGet()
+        {
+            Console.WriteLine("Getting data");
+            using (var httpClient = new HttpClient())
+            {
+                var result = await httpClient.GetStringAsync("http://backend-svc/api/values");
+                Console.WriteLine("Got " + result);
+            }
+        }
+    }
+}
